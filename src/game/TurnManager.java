3f2a94c417ebc89be2b252b1e9073acaf06a6a0e
AyalaLazaro05 <lazaritoayala05@gmail.com>
2025-04-05 22:36:47 +0200

    Reposicionando algunas clases dentro de sus paquetes correspondientes

---
+++ src/game/TurnManager.java
@@ -0,0 +1,12 @@
+package game;
+
+import java.util.List;
+
+public class TurnManager {
+    private List<Character> turnOrder;
+    private int currentTurnIndex;
+
+    public Character getCurrentCharacter() { return null; }
+    public void nextTurn() {}
+    public void resetTurn() {}
+}
