e1ad42b76ff4b704797c274b202f46f6b6475581
cortesEduardo <corteseduardo051205@gmail.com>
2025-04-10 09:49:54 -0400

    Delete src/characters/EnemysCharacters.java

--- src/characters/EnemysCharacters.java
+++
@@ -1,9 +0,0 @@
-package characters;
-
-public class EnemysCharacters extends Character{
-
-
-    public EnemysCharacters(String nombre, int salud, int ataque, int defensa, int movement, int position) {
-        super(nombre, salud, ataque, defensa, movement,position);
-    }
-}
