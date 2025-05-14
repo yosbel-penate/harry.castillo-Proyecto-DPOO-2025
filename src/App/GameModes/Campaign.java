962b64de36c6baa03a41edb31d111393ece1a572
cortesEduardo <corteseduardo051205@gmail.com>
2025-05-13 19:15:31 -0600

    Add modos de juego

---
+++ src/App/GameModes/Campaign.java
@@ -0,0 +1,11 @@
+package app.gameModes;
+
+import app.gameplayFeatures.Gameplay;
+// Variables estaticas importadas.
+
+public class Campaign {
+    public static void initialize() {
+        Gameplay.initializeGameplay();
+
+    }
+}
