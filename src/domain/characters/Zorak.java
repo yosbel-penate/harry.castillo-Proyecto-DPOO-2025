9d1d6154b1e479a10aa7e906a6f785ed41122dc7
XavierAlfonso2005 <borgesleanet@gmail.com>
2025-06-02 12:50:09 -0400

    Update Zorak.java

--- src/domain/characters/Zorak.java
+++ src/domain/characters/Zorak.java
@@ -6,8 +6,8 @@ public class Zorak extends PlayerCharacter {
     private int health = 8;
     private int attack = 6;
     private boolean havesMana = false;
-    private String characterName = "Zorak";
-    private String imageName = "Zorak.png";
+    private String characterName = "zorak";
+    private String imageName = "zorak.png";
     private String closestImageName = "closerZorak.png";
 
     public String getClosestImageName() {
